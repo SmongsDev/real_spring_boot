31921b4b34a3117150f48797603626868a79f798
Mollang2 <62112908+javascriptKR@users.noreply.github.com>
2022-12-21 14:28:39 +0000

    12/21 환경설정 중

---
+++ guestbook/src/main/java/com/example/guestbook/controller/GuestbookController.java
@@ -0,0 +1,19 @@
+package com.example.guestbook.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/guestbook")
+@Log4j
+public class GuestbookController{
+
+  @GetMapping({"/", "/list"})
+  public String list(){
+    log.info("list.......");
+
+    return "/guestbook/ist";
+  }
+}
