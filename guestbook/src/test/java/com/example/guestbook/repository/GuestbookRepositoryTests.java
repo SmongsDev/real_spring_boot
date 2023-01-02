9a5300170759e89c53d68e024092140c67a43093
Mollang2 <62112908+javascriptKR@users.noreply.github.com>
2023-01-02 14:46:58 +0000

    01/02 Querydsl TEST

--- guestbook/src/test/java/com/example/guestbook/repository/GuestbookRepositoryTests.java
+++ guestbook/src/test/java/com/example/guestbook/repository/GuestbookRepositoryTests.java
@@ -67,4 +67,31 @@ public class GuestbookRepositoryTests {
       System.out.println(guestbook);
     });
   }
+
+  @Test
+  public void testQuery2(){
+    Pageable pageable = PageRequest.of(0, 10, Sort.by("gno").descending());
+
+    QGuestbook qGuestbook = QGuestbook.guestbook;
+
+    String keyword = "1";
+
+    BooleanBuilder builder = new BooleanBuilder();
+
+    BooleanExpression exTitle = qGuestbook.title.contains(keyword);
+
+    BooleanExpression exContent = qGuestbook.content.contains(keyword);
+
+    BooleanExpression exAll = exTitle.or(exContent);
+
+    builder.and(exAll);
+
+    builder.and(qGuestbook.gno.gt(0L)); // 0보다 크다 ( gt() = 크다 [QueryDSL Grammer])
+
+    Page<Guestbook> result = guestbookRepository.findAll(builder, pageable);
+
+    result.stream().forEach(guestbook -> {
+      System.out.println(guestbook);
+    });
+  }
 }
