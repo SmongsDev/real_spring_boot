6dddd0519d1a97d9b1cc508c46a3d04d7e8b3b27
Mollang2 <62112908+javascriptKR@users.noreply.github.com>
2023-02-14 14:54:37 +0000

    02/14 파일 업로드

---
+++ guestbook/src/main/java/com/example/guestbook/dto/UploadResultDTO.java
@@ -0,0 +1,26 @@
+package com.example.guestbook.dto;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UploadResultDTO implements Serializable{
+  
+  private String fileName;
+  private String uuid;
+  private String folderPath;
+
+  public String getImageURL(){
+    try{
+      return URLEncoder.encode(folderPath+"/"+uuid+"_"+fileName, "UTF-8");
+    } catch(UnsupportedEncodingException e){
+      e.printStackTrace();
+    }
+    return "";
+  }
+}
